e6ae6bb261287288445f19de0bef0299b62bf9c3
Your Name <you@example.com>
2018-03-11 12:16:00 +0300

    Headers

--- Meal.js
+++ Meal.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export default class Meal extends React.Component {
+    static navigationOptions = {
+        title: `Meal`,
+    };
+
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
